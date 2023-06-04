401af52be52cd19a3e6024f713cec2084b5eb6fa
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-06-04 15:13:30 +0545

    feat: create console types

---
+++ src/utils/consoleTypes.ts
@@ -0,0 +1,13 @@
+import { IndividualConsoleType } from "../ts/interfaces";
+
+export const consoleTypes: Array<IndividualConsoleType> = [
+    {
+        name: "Logs",
+        id: 1
+    },
+
+    {
+        name: "Errors",
+        id: 2
+    }
+]
